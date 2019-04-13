3a98da6097cd7d1fb9b57c2498d1f80ad292dabb
CamillaRossi <camillarossi81@gmail.com>
2019-04-13 08:47:39 +0200

    Prima versione del codice della Registrazione

---
+++ Registrazione/Registrazione/src/Random.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+public class Random implements Serializable{
+	
+	public Random() {
+		
+	}
+	public Random(long seed)
+	{
+		 Random rnd = new Random();
+		 rnd.setSeed(seed);
+	}
+	public void setSeed(long seed)
+	{
+		
+	}
+
+}
